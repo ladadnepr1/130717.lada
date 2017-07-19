e930d59fc840066773407f8da1c19d4e034871da
ladadnepr1 <ladadnepr1@gmail.com>
2017-07-19 18:57:34 +0300

    add space

---
+++ registr.php
@@ -0,0 +1,9 @@
+<form method="post" action="index.php">
+    <label>name:
+        <input type="text" name="name" required/>
+    </label>
+    <label>email:
+        <input type="email" name="email" required/>   
+    </label>
+    <input type="submit" name="reg" value="add"/>
+</form>
